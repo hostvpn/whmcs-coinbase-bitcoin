f9b27fe9b97127aad77fe5d1dfe397cdcbb3b243
Ben C <ben@hl9.net>
2014-03-03 00:06:38 -0600

    Updated to use API Key + Secret auth, updated whmcs's admin instructions wording
    * Changed coinbase_post_json() to use proper HMAC-SHA256 signed API calls
    * Added api key/secret param ($api_auth) to callers of coinbase_post_json()
    * Updated instructions provided at WHMCS admin panel

--- modules/gateways/coinbase.php
+++ modules/gateways/coinbase.php
@@ -10,11 +10,12 @@ function coinbase_config() {
     "FriendlyName" => array("Type" => "System", "Value"=>"Coinbase (Bitcoin)"),
     "whmcs_admin_username" => array("FriendlyName" => "WHMCS Admin User Name", "Type" => "text", "Size" => "20", "Description" => "Need an admin user name to use the local API, usually it's just 'admin'.", ),
     "coinbase_api_key" => array("FriendlyName" => "Coinbase API Key", "Type" => "password", "Size" => "70", ),
+    "coinbase_api_secret" => array("FriendlyName" => "Coinbase API Secret", "Type" => "password", "Size" => "70", ),
     "coinbase_type" => array("FriendlyName" => "Coinbase Button Type", "Type" => "dropdown", "Options" => "buy_now,donation,subscription", "Description" => "Subscription type is not yet available.", ),
     "coinbase_style" => array("FriendlyName" => "Coinbase Button Style", "Type" => "dropdown", "Options" => "buy_now_small,buy_now_large,donation_small,donation_large,custom_small,custom_large", ),
     "coinbase_text" => array("FriendlyName" => "Coinbase Button Text", "Type" => "text", "Size" => "70", "Description" => "<br />This is the text that will appear on your button (e.g. 'Pay with Bitcoin')." ),
-    "coinbase_ca_path" => array("FriendlyName" => "Coinbase CA Cert Path", "Type" => "text", "Size" => "70", "Description" =>"<br />This should be the full directory path to the ca-coinbase.crt file (e.g. /home/username/public_html/whmcs/modules/gateways/coinbase/ca-coinbase.crt)." ),
-    "callback_secret" => array("FriendlyName" => "Coinbase Callback Secret", "Type" => "text", "Size" => "70", "Description" => "<br />Secret key will be appended to the end of your callback URL (e.g. https://yourdomain/modules/gateways/callback/coinbase.php?secret=) which you will need to provide to Coinbase so they can notify WHMCS of payments." ),
+    "coinbase_ca_path" => array("FriendlyName" => "Coinbase CA Cert Path", "Type" => "text", "Size" => "70", "Description" =>"<br />This should be the full directory path to the ca-coinbase.crt file (e.g. " . dirname(__FILE__) . "/coinbase/ca-coinbase.crt)." ),
+    "callback_secret" => array("FriendlyName" => "Coinbase Callback Secret", "Type" => "text", "Size" => "70", "Description" => "<br />You will need to append your callback secret to the callback URL you supply to Coinbase.  For example, a Callback Secret of 'myKey000', then supply Coinbase with the Callback URL of:<br />https://yourdomain/modules/gateways/callback/coinbase.php?secret=myKey000" ),
   );
   return $configarray;
 }
@@ -22,8 +23,8 @@ function coinbase_config() {
 function coinbase_link($params) {
 
   # Gateway Specific Variables
-  $api_key  = $params['coinbase_api_key'];
-  $url      = 'https://coinbase.com/api/v1/buttons?api_key=' . $api_key;
+  $api_auth = array($params['coinbase_api_key'], $params['coinbase_api_secret']);
+  $url      = 'https://coinbase.com/api/v1/buttons';
   $type     = $params['coinbase_type'];
   $style    = $params['coinbase_style'];
   $text     = $params['coinbase_text'];
@@ -52,7 +53,7 @@ function coinbase_link($params) {
   $systemurl   = $params['systemurl'];
   $currency    = $params['currency'];
 
-  $code = coinbase_button_request($invoiceid, $amount, $currency, $description, $type, $style, $text, $url, $ca_path);
+  $code = coinbase_button_request($invoiceid, $amount, $currency, $description, $type, $style, $text, $url, $ca_path, $api_auth);
 
   return $code;
 
@@ -107,7 +108,7 @@ function coinbase_link($params) {
 
 }*/
 
-function coinbase_button_request($invoiceid, $amount, $currency, $description, $type, $style, $text, $url, $ca_path) {
+function coinbase_button_request($invoiceid, $amount, $currency, $description, $type, $style, $text, $url, $ca_path, $api_auth) {
 
   $button_data = array(
     'button'=>array(
@@ -120,7 +121,7 @@ function coinbase_button_request($invoiceid, $amount, $currency, $description, $
       'style'=>$style),
     );
 
-  $button_response = coinbase_post_json($url, $button_data, $ca_path);
+  $button_response = coinbase_post_json($url, $button_data, $ca_path, $api_auth);
 
   $button      = $button_response['button'];
   $button_code = $button['code'];
@@ -136,12 +137,30 @@ function coinbase_button_request($invoiceid, $amount, $currency, $description, $
 
 }
 
-function coinbase_post_json($url, $button_data, $ca_path) {
+function coinbase_post_json($url, $button_data, $ca_path, $api_auth) {
+
+  if(!function_exists('hash_hmac')) { 
+    die("The hash_hmac() function is unavailable.  Please upgrade PHP.");
+  }
+
+  list($api_key, $api_secret) = $api_auth;
+
+  // Same method as the official coinbase library for nonce
+  $microseconds = sprintf('%0.0f',round(microtime(true) * 1000000));
+  $to_sign = $microseconds . $url . json_encode($button_data);
+  $signature = hash_hmac("sha256", $to_sign, $api_secret);
+
+  $add_headers = array(
+    'Content-Type: application/json',
+    'ACCESS_KEY: ' . $api_key,
+    'ACCESS_SIGNATURE: ' . $signature,
+    'ACCESS_NONCE: ' . $microseconds,
+  );
 
   //$ca_coinbase_path = getcwd() . '/coinbase/ca-coinbase.crt';
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $add_headers);
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_TIMEOUT, 30);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
